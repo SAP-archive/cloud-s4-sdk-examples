673d5c7efaec76bb7ca43a548e869879b66fdbbc
Marika Marszalkowski <marika.marszalkowski@sap.com>
2019-08-01 18:08:44 +0200

    Very minor example on systems.json

--- src/hello-world-route.ts
+++ src/hello-world-route.ts
@@ -1,5 +1,18 @@
+import { Destination, executeHttpRequest, HttpMethod } from "@sap/cloud-sdk-core";
 import { Request, Response } from "express";
 
-export function helloWorld(req: Request, res: Response) {
-  res.status(200).send("Hello, World!");
+export async function helloWorld(req: Request, res: Response) {
+  await doSomething({
+    url: "http://example.com"
+  }).then((greeting: string) => {
+    res.status(200).send(`${greeting}, World!`);
+  });
+}
+
+export function doSomething(destination: Destination) {
+  return executeHttpRequest(destination, {
+    method: HttpMethod.GET
+  })
+  .then(() => "Hello")
+  .catch(() => "Bye");
 }
